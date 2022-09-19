34c5cef4059f93733266463c88d3e3ba586eaaa2
kevin liu <liveonnet@qq.com>
2022-09-19 15:18:01 +0800

    misc

---
+++ include/private_info.h
@@ -0,0 +1,12 @@
+#ifndef _PRIVATE_INFO_H_
+#define _PRIVATE_INFO_H_
+// ===========================
+// Enter your WiFi credentials
+// ===========================
+extern const char *ssid;
+extern const char *password;
+
+// 晚上在 [END_HOUR, START_HOUR) 直接长时间休眠
+extern const int END_HOUR;
+extern const int START_HOUR;
+#endif
